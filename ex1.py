5ffefa700c4a3def0084686a6de1ffb5bd9ed9b8
Rimesh <rimesh.j@gmail.com>
2017-03-01 17:08:14 +0530

    Exercise 1 - A Good First Program
    Trying to learn how to print using print command. It can accept strings in double quotes as well as single quote.

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# Title: Exercise 1 - A Good First Program
+# from Learn Python the hard way
+# Date: 1st March 2017
+# by Rimesh Jotaniya
+# Description: Trying to learn how to print using print command. It can accept strings in double quotes as well as single quote.
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun"
+print "Yay! printing."
+print "I'd much rather you 'not'."
+print 'I said do "not" touch this.'
+
