e527a5ff6bb9a3efa55e065e4711d427e5168011
Rimesh <rimesh.j@gmail.com>
2017-03-01 17:09:07 +0530

    Exercise 2 - Comments and Pound Characters
    Comments using # character

---
+++ ex2.py
@@ -0,0 +1,16 @@
+# Title: Exercise 2 - Comments and Pound Characters
+# from Learn Python the hard way
+# Date: 1st March 2017
+# by Rimesh Jotaniya
+# Description: Comments using # character
+
+
+# A comment, This is so you can read your program later
+# Anything after # is ignored by python
+
+print "I could have code like this" # and the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out a piece of code.
+# print "This won't run"
+
+print "This will run"
