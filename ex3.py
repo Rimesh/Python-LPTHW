3e592b75e7abb1a841787988502e55491a314ff2
Rimesh <rimesh.j@gmail.com>
2017-03-01 17:09:54 +0530

    Exercise 3 - Numbers and Math
    Order of evaluation: [PEMDAS] : [Parentheses Exponents Multiplication Division Addition Subtraction]

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# Title: Exercise 3 - Numbers and Math
+# from Learn Python the hard way
+# Date: 1st March 2017
+# by Rimesh Jotaniya
+# Description: Order of evaluation: [PEMDAS] : [Parentheses Exponents Multiplication Division Addition Subtraction]
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6 # division first then addition 25 + (30 / 6)
+print "Roosters", 100 - 25 * 3 % 4 # 
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it grater or equal?", 5 >= -2
+print "Is it less or equal?", 5<= -2
+
+print "7 / 4 = ", 7 / 4
+print "7.0 / 4.0 = ", 7.0 / 4.0
