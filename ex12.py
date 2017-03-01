3605a11c478a4d5b44dab9a52df20075c2a091fc
Rimesh <rimesh.j@gmail.com>
2017-03-01 17:32:26 +0530

    Exercise 12 - Prompting People
    raw input with prompt

---
+++ ex12.py
@@ -0,0 +1,11 @@
+# Title: Exercise 12 - Prompting People
+# from Learn Python the hard way
+# Date: 1st March 2017
+# by Rimesh Jotaniya
+# Description: raw input with prompt
+
+age = raw_input("How old are you? ")
+height = raw_input("how tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
