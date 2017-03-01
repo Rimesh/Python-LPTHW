bc6338ead9cd5e2a71eea7c6dd6f7393a97bfd71
Rimesh <rimesh.j@gmail.com>
2017-03-01 17:15:26 +0530

    Exercise 9 - Printing, Printing, Printing
    Two ways to print a string in multiple lines. 1st using \n and 2nd using """
    \n adds new line. use of Three double-quotes for printing multiple lines.

---
+++ ex9.py
@@ -0,0 +1,19 @@
+# Title: Exercise 9 - Printing, Printing, Printing
+# from Learn Python the hard way
+# Date: 1st March 2017
+# by Rimesh Jotaniya
+# Description: Two ways to print a string in multiple lines. 1st using \n and 2nd using """
+# \n adds new line. use of Three double-quotes for printing multiple lines.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There is something going on here.
+With the three double-quotes.
+We'll be able to write as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
