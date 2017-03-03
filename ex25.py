53580ab66c7c3735c0acc0d5a7519bee99fddf83
Rimesh <rimesh.j@gmail.com>
2017-03-03 12:59:31 +0530

    Exercise 25 - Even More Practice
    This file will be imported for taking test!

---
+++ ex25.py
@@ -0,0 +1,42 @@
+# Title: Exercise 25 - Even More Practice
+# from Learn Python the hard way
+# Date: 3rd March 2017
+# by Rimesh Jotaniya
+# Description: This file will be imported for taking test!
+
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print word
+
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print word
+
+def sort_sentence(sentence):
+    """Takes in full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """prints first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+    
+def print_first_and_last_sorted(sentence):
+    """Sorts the words, then prints first and last word."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
