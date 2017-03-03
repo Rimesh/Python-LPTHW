74075b68a2832b01c98b68a924d03c9a240991fe
Rimesh <rimesh.j@gmail.com>
2017-03-03 15:29:09 +0530

    Exercise 30 - Else and If
    Introduction to elif

---
+++ ex30.py
@@ -0,0 +1,28 @@
+# Title: Exercise 30 - Else and If
+# from Learn Python the hard way
+# Date: 3rd March 2017
+# by Rimesh Jotaniya
+# Description: Introduction to elif
+
+people = 30
+cars = 40
+buses = 15
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+    
+if buses > cars:
+    print "That's too many buses."
+elif buses < cars:
+    print "May be we could take the buses."
+else:
+    print "We still can't decide."
+
+if people > buses:
+    print "Alright, let's just take the buses."
+else:
+    print "Fine, let's stay home then."
